e13a70c29920e8cb183f6a3eb1cfff28031369ca
themayurthakare <mayurthakare3000@gmail.com>
2025-11-17 17:20:13 +0530

    Initial Commit

---
+++ src/main/java/com/mmt/campuscart/CampuscartApplication.java
@@ -0,0 +1,13 @@
+package com.mmt.campuscart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampuscartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampuscartApplication.class, args);
+	}
+
+}
