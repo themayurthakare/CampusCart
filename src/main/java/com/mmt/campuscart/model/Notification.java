085a40bb4c44a687f059655ae038d6ba4e7b3aa2
themayurthakare <mayurthakare3000@gmail.com>
2025-11-21 11:43:05 +0530

    added fields for notification class

--- src/main/java/com/mmt/campuscart/model/Notification.java
+++ src/main/java/com/mmt/campuscart/model/Notification.java
@@ -1,4 +1,25 @@
 package com.mmt.campuscart.model;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "notifications")
 public class Notification {
+
+    @Id
+    private String id;
+
+    private String userId;
+    private String message;
+
+    private Boolean isRead;
+
+    private LocalDateTime createdAt;
 }
