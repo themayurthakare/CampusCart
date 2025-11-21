102f3e3e081ddc6536e59e2f38a657cb5d312d8e
themayurthakare <mayurthakare3000@gmail.com>
2025-11-21 11:39:28 +0530

    Added fields for Product class

--- src/main/java/com/mmt/campuscart/model/Product.java
+++ src/main/java/com/mmt/campuscart/model/Product.java
@@ -1,4 +1,35 @@
 package com.mmt.campuscart.model;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "products")
 public class Product {
+
+    @Id
+    private String id;
+
+    private String sellerId;
+
+    private String title;
+    private String description;
+    private String category;
+    private Double price;
+
+    private String condition;
+    private String department;
+
+    private List<String> images;
+
+    private String status; // available, sold, pending, rejected
+
+    private LocalDateTime createdAt;
 }
