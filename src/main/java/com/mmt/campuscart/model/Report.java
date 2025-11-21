0f846f791103f86b80733a334b9d588129be56b2
themayurthakare <mayurthakare3000@gmail.com>
2025-11-21 11:43:48 +0530

    added fields for report class

--- src/main/java/com/mmt/campuscart/model/Report.java
+++ src/main/java/com/mmt/campuscart/model/Report.java
@@ -1,4 +1,30 @@
 package com.mmt.campuscart.model;
 
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "reports")
 public class Report {
+
+    @Id
+    private String id;
+
+    private String reporterId;
+    private String reportedUserId;
+    private String productId;
+
+    private String reason;
+    private String details;
+
+    private String status;
+
+    private LocalDateTime createdAt;
 }
