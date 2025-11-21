e195b82a04f4b0d769ad8d7577807aa7d033155e
themayurthakare <mayurthakare3000@gmail.com>
2025-11-21 11:41:45 +0530

    added fields to message

--- src/main/java/com/mmt/campuscart/model/Message.java
+++ src/main/java/com/mmt/campuscart/model/Message.java
@@ -1,4 +1,20 @@
 package com.mmt.campuscart.model;
 
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Message {
+
+    private String senderId;
+    private String text;
+    private List<String> attachments;
+
+    private LocalDateTime sentAt;
 }
