910d371eb32df143dc1e50c4276933a9cfd10fc7
themayurthakare <mayurthakare3000@gmail.com>
2025-11-21 11:42:31 +0530

    added fields to chat class

--- src/main/java/com/mmt/campuscart/model/Chat.java
+++ src/main/java/com/mmt/campuscart/model/Chat.java
@@ -1,4 +1,22 @@
 package com.mmt.campuscart.model;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "chats")
 public class Chat {
+
+    @Id
+    private String id;
+
+    private List<String> participants;
+
+    private List<Message> messages;
 }
