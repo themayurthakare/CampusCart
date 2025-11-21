235462fb466ef250f7630c48a238ae0e9fce7285
themayurthakare <mayurthakare3000@gmail.com>
2025-11-21 11:40:06 +0530

    Added fields for Wishlist class

--- src/main/java/com/mmt/campuscart/model/Wishlist.java
+++ src/main/java/com/mmt/campuscart/model/Wishlist.java
@@ -1,4 +1,22 @@
 package com.mmt.campuscart.model;
 
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "wishlist")
 public class Wishlist {
+
+    @Id
+    private String id;
+
+    private String userId;
+    private List<String> productIds;
 }
