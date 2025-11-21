3f210b4d470b894f032041c3a9d9d34f9913a868
themayurthakare <mayurthakare3000@gmail.com>
2025-11-21 11:21:02 +0530

    Defined all the fields that are usefull for user

--- src/main/java/com/mmt/campuscart/model/User.java
+++ src/main/java/com/mmt/campuscart/model/User.java
@@ -1,4 +1,35 @@
 package com.mmt.campuscart.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "users")
 public class User {
+    @Id
+    private String id;
+
+    private String name;
+    private String email;
+    private String password;
+
+    private String role; // STUDENT, ALUMNI, ADMIN
+    private String department;
+    private Integer graduationYear;
+
+    private String profileImage;
+    private String phone;
+
+    private String status; // active, suspended
+
+    private LocalDateTime createdAt;
 }
